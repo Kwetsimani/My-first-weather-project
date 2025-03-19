b2a3ec9ea3773f74df5682e9cf3034068fabe563
Kwetsimani <kwetsimanx@gmail.com>
2025-03-20 00:18:26 +0200

    added the basic engine behavior

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
